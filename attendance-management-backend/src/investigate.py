81ca30ec54803faf73db3317de24d14d1363619d
Kanishka Dharmasena <73683447+KanishkaDharmasena@users.noreply.github.com>
2023-09-14 23:15:04 +0530

    initial changes

--- attendance-management-backend/src/investigate.py
+++ attendance-management-backend/src/investigate.py
@@ -1,1 +1,7 @@
+from PIL import Image, ImageOps, ImageChops
 
+def rmsd(image1, image2):
+    diff = ImageChops.difference(image1, image2)
+    h = diff.histogram()
+    rms = (sum(h) / (image1.size[0] * image1.size[1])) ** 0.5
+    return rms
