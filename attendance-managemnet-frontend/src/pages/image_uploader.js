cf5bf3cbc5e126527cc71b02270f31edf159522c
Kanishka Dharmasena <73683447+KanishkaDharmasena@users.noreply.github.com>
2023-09-14 23:28:26 +0530

    Update image_uploader.js

--- attendance-managemnet-frontend/src/pages/image_uploader.js
+++ attendance-managemnet-frontend/src/pages/image_uploader.js
@@ -2,10 +2,6 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useDropzone } from 'react-dropzone';
 
-import React, { useState } from 'react';
-import axios from 'axios';
-import { useDropzone } from 'react-dropzone';
-
 const ImageUploader = () => {
   const [uploadedImage, setUploadedImage] = useState(null);
 
