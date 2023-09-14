03c10ea586dded2633456c43191f10a1839f9f82
Kanishka Dharmasena <73683447+KanishkaDharmasena@users.noreply.github.com>
2023-09-14 23:16:00 +0530

    Code addition

--- attendance-management-backend/src/investigate.py
+++ attendance-management-backend/src/investigate.py
@@ -5,3 +5,8 @@ def rmsd(image1, image2):
     h = diff.histogram()
     rms = (sum(h) / (image1.size[0] * image1.size[1])) ** 0.5
     return rms
+template_path = 'template_signature.png'
+student_signature_path = 'student_signature.png'
+
+template_image = Image.open(template_path).convert('L')
+student_image = Image.open(student_signature_path).convert('L')
