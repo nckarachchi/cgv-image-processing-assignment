d10a7634ef510cfb2c02cd18ad1c42990d3e8f15
supun731 <assgunathilaka@students.nsbm.ac.lk>
2023-09-15 21:30:14 +0530

    Update Server

--- attendance-managemnet-frontend/src/pages/server.js
+++ attendance-managemnet-frontend/src/pages/server.js
@@ -3,7 +3,7 @@ const fileUpload = require('express-fileupload');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 1000;
 
 app.use(cors());
 
