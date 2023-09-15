afeaeb710cd359c63f07d8a122f0a3f199c3e044
Kanishka Dharmasena <73683447+KanishkaDharmasena@users.noreply.github.com>
2023-09-15 10:15:25 +0530

    Minor fixes

--- attendance-managemnet-frontend/src/App.js
+++ attendance-managemnet-frontend/src/App.js
@@ -1,1 +1,31 @@
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+     
+        
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+       attendance management system
+        </a><br></br>
+        <div style={{paddingLeft:600}}>    
+        <button>image upload</button>
+        
+      <button>attendance report</button>
+      </div>
+    
+      </header>
+    
+    </div>
+  );
+}
+
+export default App;
 
